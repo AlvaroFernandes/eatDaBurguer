a0ebbb5a9468b3127a8cdc8af6b24b3272db69d1
Alvaro Fernandes <fernandes.alvaro@gmail.com>
2020-08-20 18:37:07 +1000

    initial commit

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name,devoured) VALUES ("Beef Burger with Bacon",true);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Beef Burger with Double Cheese",false);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Chicken Crispy Burger",true);
