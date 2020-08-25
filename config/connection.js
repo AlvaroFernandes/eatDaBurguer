1d772cf726706dc0dde81850ca3f7ea9b1444dec
Alvaro Fernandes <fernandes.alvaro@gmail.com>
2020-08-26 08:44:48 +1000

    update connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@
 const mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
+    mysql.createConnection({
         host: "localhost",
         port: 3306,
         user: "root",
